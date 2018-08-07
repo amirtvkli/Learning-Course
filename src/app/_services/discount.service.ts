8ab8a9be21b49050adcaecd2a1ae00ec2f647462
Amir Tavakoli <amirtavakoli@outlook.com>
2018-08-07 18:14:31 +0430

    minor improvment

--- src/app/_services/discount.service.ts
+++ src/app/_services/discount.service.ts
@@ -11,8 +11,11 @@ export class DiscountService {
     getAmount(couponCode,courseId){
         try {
             return this.http.get(AppConfig.BASE_URL+`/registration/couponvalidate/${couponCode}/${courseId}`)
-            .map((res: Response) => res.json() )  // could raise an error if invalid JSON
-     
+            .map((res: Response) => res.json())  // could raise an error if invalid JSON
+        }
+        catch(error){
+
+        }
+        finally{}
     }
-   
 }
